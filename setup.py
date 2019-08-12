9feb7c50b0e6d5ffc86132bedf4c2c713a02b296
Mads Hansen <mads.hansen@marklogic.com>
2019-08-11 21:08:20 -0400

    formatting, and bump version

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from codecs import open
 from os.path import dirname, join
 from setuptools import setup, find_packages
 
+
 def main():
     base_dir = dirname(__file__)
     setup(
@@ -11,7 +12,7 @@ def main():
         name='newrelic_marklogic_plugin',
         description='NewRelic plugin for monitoring MarkLogic.',
         long_description=open(join(base_dir, 'README.rst'), encoding='utf-8').read(),
-        version='0.2.8',
+        version='0.2.9',
         packages=find_packages(),
         url='https://github.com/marklogic-community/newrelic-plugin',
         license=open(join(base_dir, 'LICENSE'), encoding='utf-8').read(),
@@ -25,15 +26,16 @@ def main():
                      'License :: OSI Approved :: Apache Software License',
                      'Operating System :: OS Independent',
                      'Topic :: Software Development :: Libraries :: Python Modules'
-                     ],
+                    ],
         scripts=[
             'scripts/newrelic_marklogic.py'
         ],
         platforms='any',
         install_requires=[
             'requests>=2.11.1'
-        ],
+        ]
     )
+
 
 if __name__ == '__main__':
     main()
