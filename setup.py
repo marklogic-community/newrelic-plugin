e3bfb4f9dcdf34b3debc149621b6443a135ea815
Mads Hansen <mads.hansen@marklogic.com>
2019-12-26 11:19:11 -0500

    Adjust how README.rst is read and content-type declaration

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ except ImportError:
 import newrelic_marklogic_plugin
 
 
+def read(fname):
+    return open(join(dirname(__file__), fname)).read()
+
 def main():
-    base_dir = dirname(__file__)
     setup(
-        base_dir=dirname(__file__),
         name='newrelic_marklogic_plugin',
         description='New Relic plugin for monitoring MarkLogic.',
-        long_description=open(join(base_dir, 'README.rst'), encoding='utf-8').read(),
-        long_description_content_type='text/markdown',
+        long_description=read('README.rst'),
+        long_description_content_type='text/x-rst',
         version=newrelic_marklogic_plugin.__version__,
         packages=find_packages(),
         url='https://github.com/marklogic-community/newrelic-plugin',
